6f1dda84d2c7bd84ff84e202f791335c0bd72814
Brad Kimmel <intrepidca@gmail.com>
2024-10-20 15:12:21 -0400

    Add production rules that transform pattern.

--- lute/cli/korean_term_analysis.py
+++ lute/cli/korean_term_analysis.py
@@ -20,6 +20,7 @@ from lute.db import db
 from lute.models.book import Book
 from lute.term.model import Repository
 from mecab import MeCab, Feature
+from tqdm import tqdm
 from . import static
 
 
@@ -89,7 +90,7 @@ def normalize(features):
         elif start_pos in ['VA', 'XSA']:
             stem_pos = 'VA'
             stem_end = i
-        elif start_pos in ['VX']:
+        elif start_pos in ['VX']:  # should probably not be combined
             stem_end = i
 
     if stem_end is not None:
@@ -127,8 +128,8 @@ def parse_elem(elem):
     parts = elem.split('/', 3)
     if len(parts) != 3:
         raise ValueError('invalid element: {}'.format(elem))
-    if parts[2] != '*':
-        print("### {}".format(parts[2]))
+    # if parts[2] != '*':
+        # print("### {}".format(parts[2]))
     return (parts[0], parts[1], parts[2])
 
 
@@ -182,10 +183,22 @@ POS_TAGS = {
     # 'SN':   [],
 }
 
-HONORIFIC_ELEMS = (
-        ('으시', 'EP', '*'),
-        ('시', 'EP', '*'),
-)
+# HONORIFIC_ELEMS = (
+#         ('으시', 'EP', '*'),
+#         ('시', 'EP', '*'),
+# )
+
+
+LETTER_SUBS = [
+    ('\u1107', 'ㅂ'),
+    ('\u11b8', 'ㅂ'),
+    ('\u1105', 'ㄹ'),
+    ('\u11af', 'ㄹ'),
+    ('\u1102', 'ㄴ'),
+    ('\u11ab', 'ㄴ'),
+    ('\u1106', 'ㅁ'),
+    ('\u11b7', 'ㅁ'),
+]
 
 
 @dataclass
@@ -209,6 +222,49 @@ class Pattern:
     signature:   str
     translation: str
     tags:        list
+    parents:     list
+    output:      list
+    line:        int
+    used:        bool
+
+
+def reduce_pattern_once(elements, patterns, parents=None, tags=None, word=None):
+    for n in reversed(range(1, len(elements))):
+        for i in range(0, len(elements) - n + 1):
+            sub_pattern = '+'.join(['/'.join(x) for x in elements[i:i+n]])
+            if sub_pattern not in patterns:
+                continue
+            p = patterns[sub_pattern]
+            if p.translation and not p.output:
+                continue
+            old_pattern = '+'.join(['/'.join(x) for x in elements])
+            elements[i:i+n] = p.output
+            new_pattern = '+'.join(['/'.join(x) for x in elements])
+            if word is not None:
+                _trace(sub_pattern, "term {} has subpattern: {} => {}", word, old_pattern, new_pattern)
+                _trace(word, "applying production rule for {} => {}", sub_pattern, new_pattern)
+            if parents is not None:
+                for parent in p.parents:
+                    if parent not in parents:
+                        if word:
+                            _trace(word, "adding parent {}", parent)
+                            _trace(parent, "added as parent to word {}", word)
+                        parents.append(parent)
+            if tags is not None:
+                for tag in p.tags:
+                    if tag not in tags:
+                        _trace(word, "adding tag {}", tag)
+                        tags.append(tag)
+            p.used = True
+            return True
+    return False
+
+
+def reduce_pattern(elements, patterns, parents=None, tags=None, word=None):
+    reduced = False
+    while reduce_pattern_once(elements, patterns, parents, tags, word):
+        reduced = True
+    return reduced
 
 
 def analyze_term(term, features, termrepo, language_id, patterns, undefined_roots, undefined_patterns):
@@ -270,49 +326,84 @@ def analyze_term(term, features, termrepo, language_id, patterns, undefined_root
         undefined_roots[root_word] = stats
         return
 
-    if elements[0][1].startswith('NN'):
-        _trace(term.text, "word begins with a noun")
-        elements[0] = (elements[0][0], 'NN', '*')
-
     pos = elements[-1][1]
     pos_tags = POS_TAGS.get(pos)
     tags = []
     if pos_tags:
         tags.extend(pos_tags)
-    old_elements = elements
-    elements = []
-    for e in old_elements:
-        if e in HONORIFIC_ELEMS:
-            _trace(term.text, "dropping honorific element {} from pattern", e)
-            tags.append('honorific')
-            continue
-        elements.append(e)
+
+    if elements[0][1].startswith('NN'):
+        _trace(term.text, "word begins with a noun")
+        elements[0] = (elements[0][0], 'NN', '*')
+
+    # old_elements = elements
+    # elements = []
+    # for e in old_elements:
+    #     if e in HONORIFIC_ELEMS:
+    #         _trace(term.text, "dropping honorific element {} from pattern", e)
+    #         tags.append('honorific')
+    #         continue
+    #     elements.append(e)
 
     if len(elements) < 2:
         _trace(term.text, "word has fewer than two elements")
         return
 
     elements[0] = ('*', elements[0][1], '*')
+    for i, e in enumerate(elements):
+        text = e[0]
+        old_text = text
+        for (x, y) in LETTER_SUBS:
+            text = text.replace(x, y)
+        elements[i] = (text, e[1], e[2])
+
     pattern = '+'.join(['/'.join(x) for x in elements])
     _trace(pattern, "term {} has this pattern", term.text)
     _trace(term.text, "has pattern {}", pattern)
     translation = None
-    if pattern in patterns:
-        p = patterns[pattern]
-        translation = p.translation
-        if term.translation is not None and term.translation != p.translation:
-            _trace(term.text, "conflicting translations: '{}' vs. '{}'", term.translation, translation)
-            if 'conflict' not in term.term_tags:
-                term.term_tags.append('conflict')
-            return
-        _trace(term.text, "updating term: status={}, parent={}, translation={}", root_term.status, root_word, translation)
-        term.status = root_term.status
-        term.parents = [root_word]
-        term.translation = translation
-        for tag in tags + p.tags:
-            if tag not in term.term_tags:
-                _trace(term.text, "adding tag: {}", tag)
-                term.term_tags.append(tag)
+    parents = [root_word]
+    while True:
+        p = patterns.get(pattern)
+        if p and p.translation:
+            p = patterns[pattern]
+            p.used = True
+            for parent in p.parents:
+                if parent not in parents:
+                    parent_term = termrepo.find(language_id, parent)
+                    if parent_term is None or parent_term.id is None:
+                        _trace(term.text, "parent term {} is undefined", parent)
+                        return
+                    parents.append(parent)
+            translation = p.translation
+            _trace(term.text, "existing translation: '{}'", term.translation)
+            if term.translation is not None:
+                _trace(term.text, "has existing translation; checking for conflicts...")
+                if term.translation != translation:
+                    _trace(term.text, "conflicting translations: '{}' vs. '{}'", term.translation, translation)
+                    if 'conflict' not in term.term_tags:
+                        term.term_tags.append('conflict')
+                    return
+                _trace(term.text, "original parents: {}, new parents: {}", term.parents, parents)
+                if term.parents != parents:
+                    _trace(term.text, "conflicting parents: '{}' vs. '{}'", term.parents, parents)
+                    if 'conflict' not in term.term_tags:
+                        term.term_tags.append('conflict')
+                    return
+            _trace(term.text, "updating term: status={}, parents={}, translation={}, tags={}", root_term.status, parents, translation, set(tags + p.tags))
+            term.status = root_term.status
+            term.parents = parents
+            term.translation = translation
+            for tag in tags + p.tags:
+                if tag not in term.term_tags:
+                    _trace(term.text, "adding tag: {}", tag)
+                    term.term_tags.append(tag)
+            break
+
+        else:
+            if not reduce_pattern_once(elements, patterns, parents, tags, term.text):
+                break
+            pattern = '+'.join(['/'.join(x) for x in elements])
+
     if not translation:
         _trace(term.text, "cannot translate word because there is no translation for pattern: {}", pattern)
         _trace(pattern, "cannot translate {} because pattern is not defined", term.text)
@@ -346,7 +437,7 @@ def analyze_word(word, is_bad, features, collector, termrepo, language_id, patte
         term.parents = []
 
     if term.id is not None and 'generated' in term.term_tags:
-        _trace(word, "generated word already exists, resetting...")
+        _trace(word, "generated word already exists, resetting: status={}, parents={}, tags={}, translation={}", term.status, term.parents, term.term_tags, term.translation)
         term.status = 0 
         term.translation = None
         term.term_tags = []
@@ -368,7 +459,7 @@ def analyze_book(book, collector, termrepo, language_id, mecab, patterns, undefi
     Process a single book.
     """
 
-    print("Analysing {}".format(book.title))
+    # print("Analysing {}".format(book.title))
 
     fulltext = "\n".join([t.text for t in book.texts])
     analyze_text(fulltext, collector, termrepo, language_id, mecab, patterns, undefined_roots, undefined_patterns)  # pylint: disable=too-many-locals
@@ -387,10 +478,10 @@ def analyze_book_file(path, collector, termrepo, language_id, mecab, patterns, u
                     title = v
                 continue
             text += line
-    if title:
-        print("Analyzing {}: {}".format(path, title))
-    else:
-        print("Analyzing {}".format(path))
+    # if title:
+    #     print("Analyzing {}: {}".format(path, title))
+    # else:
+    #     print("Analyzing {}".format(path))
     analyze_text(text, collector, termrepo, language_id, mecab, patterns, undefined_roots, undefined_patterns)  # pylint: disable=too-many-locals
 
 
@@ -450,24 +541,61 @@ def run(include_books, exclude_books, book_globs, commit, trace):
     with pattern_file.open(newline='') as f:
         r = csv.DictReader(f)
         for row in r:
+            line_no = r.line_num
             signature = row['pattern']
-            translation = row['translation']
+            translation = row.get('translation') or None
             tags = []
             if 'tags' in row and row['tags']:
                 tags = [s.strip() for s in row['tags'].split(',')]
-            _trace(signature, "defining pattern: translation={}, tags={}", translation, tags)
+            parents = []
+            if 'parents' in row and row['parents']:
+                parents = [s.strip() for s in row['parents'].split(',')]
+            output = []
+            if 'output' in row and row['output']:
+                output = split_expr(row['output'])
+            for (x, _) in LETTER_SUBS:
+                if x in signature:
+                    print(f"WARNING: Line {line_no}: invalid character '{x}' in rule for pattern {signature}")
+                s = row.get('output') or ''
+                if x in s:
+                    print(f"WARNING: Line {line_no}: invalid character '{x}' in output '{s}' in rule for pattern {signature}")
+            if signature in patterns:
+                print(f"WARNING: Line {line_no}: redefining pattern on line {patterns[signature].line}: {signature}")
+                continue
+            _trace(signature, "defining pattern: translation={}, tags={}, parents={}", translation, tags, parents)
             patterns[signature] = Pattern(
                 signature=signature,
                 translation=translation,
                 tags=tags,
+                parents=parents,
+                output=output,
+                line=line_no,
+                used=False,
             )
+
+    for sig, pat in patterns.items():
+        e = split_expr(sig)
+        if reduce_pattern(e, patterns):
+            new_sig = '+'.join(['/'.join(x) for x in e])
+            new_pat = patterns.get(new_sig)
+            print(f"WARNING: Line {pat.line}: pattern {sig} reduces to {new_sig}")
+            if new_pat:
+                print(f"  >> Reduced pattern defined on line {new_pat.line}")
+                if new_pat.translation == pat.translation:
+                    print(f"  >> Reduced pattern has same translation")
+                else:
+                    print(f"  >> Reduced pattern has different translation:")
+                    print(f"    >> Original translation: {pat.translation}")
+                    print(f"    >> Reduced translation : {new_pat.translation}")
+            else:
+                print("  >> Reduced pattern not defined")
 
     mecab = MeCab()
     repo = Repository(db)
     terms = {}
     undefined_roots = {}
     undefined_patterns = {}
-    for b in books:
+    for b in tqdm(books, desc="processing books"):
         analyze_book(b, terms, repo, langid, mecab, patterns, undefined_roots, undefined_patterns)
 
     for spec in book_globs:
@@ -482,7 +610,7 @@ def run(include_books, exclude_books, book_globs, commit, trace):
     num_untranslatable = 0
     num_deleted = 0
     num_patched_parents = 0
-    for term in terms.values():
+    for term in tqdm(terms.values(), desc="processing terms"):
         if 'fixme' in term.term_tags:
             if not term.translation:
                 _trace(term.text, "deleting term with fixme tag and no translation")
@@ -501,9 +629,8 @@ def run(include_books, exclude_books, book_globs, commit, trace):
             _trace(term.text, "conflicting definitions")
             num_conflicts += 1
             continue
-        if len(term.parents) != 1:
-            print("Word {} has invalid number of parents: [{}]".format(term.text, ", ".join(term.parents)))
-            _trace(term.text, "invalid number of parents")
+        if len(term.parents) == 0:
+            _trace(term.text, "no parents")
             num_invalid_parent_count += 1
             continue
         if not term.translation:
@@ -517,7 +644,7 @@ def run(include_books, exclude_books, book_globs, commit, trace):
         else:
             op = "added"
             num_added += 1
-        _trace(term.text, "{} term with parent={}, translation={}", op, term.parents[0], term.translation)
+        _trace(term.text, "{} term with parents={}, translation={}, tags={}", op, term.parents, term.translation, term.term_tags)
         repo.add(term)
 
     with open("korean_unknown_terms.csv", 'w', newline='') as f:
@@ -556,3 +683,8 @@ def run(include_books, exclude_books, book_globs, commit, trace):
     print("Stats: conflicts={}, invalid={}, untranslatable={}, unknown roots={}, added={}, updated={}, deleted={}, removed_fixme_tag={}".format(
         num_conflicts, num_invalid_parent_count, num_untranslatable, len(undefined_roots), num_added, num_updated, num_deleted, num_patched_parents,
     ))
+
+    # for sig, pat in patterns.items():
+    #     if pat.used:
+    #         continue
+    #     print(f"UNUSED PATTERN: Line {pat.line}: {sig}")
